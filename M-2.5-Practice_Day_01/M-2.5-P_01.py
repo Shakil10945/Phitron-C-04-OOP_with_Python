f8c67adba65a52519b0e74477cc3d7ec87897745
Shakil Khan <shakil10945@gmail.com>
2024-01-29 09:52:23 +0600

    first P_1

---
+++ M-2.5-Practice_Day_01/M-2.5-P_01.py
@@ -0,0 +1,10 @@
+# Lext x is the input given by the users
+x = float(input())
+
+#find  a method for floor
+floor = int(x)
+
+# find a  method for ceil
+ceil = x -floor
+
+print(f'The floor of the given number {x} is {floor} and ceil {round(ceil,2)}')
