e08f3c04c9bcc688d27ca1a73b040410c7bbc604
Shakil Khan <shakil10945@gmail.com>
2024-01-30 10:47:12 +0600

    six

--- M-3.5-Practice_Day_02/M-3.5-P_05.py
+++ M-3.5-Practice_Day_02/M-3.5-P_05.py
@@ -1,10 +1,6 @@
 s1=input()
 s2= input()
-w =''
-for i in range (0,len(s1)):
-    w+=s1[i]
-    w+=s2[i]
-
-print(w)
+if s1 in s2:
+    print("True")
 
     
---
+++ M-3.5-Practice_Day_02/M-3.5-P_06.py
@@ -0,0 +1,6 @@
+s1=input()
+s2= input()
+if s1 in s2 or s2 in s1:
+    print("True")
+else:
+    print("False")
