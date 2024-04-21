e54cc9c9304db5ea88ed848d413f9d522ea6e738
Shakil Khan <shakil10945@gmail.com>
2024-04-22 03:10:18 +0600

    2

---
+++ M-8-Class_and_Methods/2_Add_Methods_to_your_class.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def add(self, x, y):
+        print(x+y)
+    def subtract(self, x,y):
+        print(x-y)
+
+gonona = Calculator()
+
+gonona.add(80,90)
+gonona.subtract(70,130)
